6d0e2f6236fe3d0c02b0e8d7397766cc2d8d3df8
ikigaia1998 <i202117849@cibertec.edu.pe>
2024-05-09 02:13:09 -0500

    Ingreso de POO_GAME a Feature

---
+++ src/POO_Game/src/Nissan.java
@@ -0,0 +1,8 @@
+package POO_Game.src;
+
+public class Nissan extends Auto{
+
+    public Nissan(){
+
+    }
+}
---
+++ src/POO_Game/src/Toyota.java
@@ -0,0 +1,10 @@
+package POO_Game.src;
+
+public class Toyota extends Auto{
+
+    Toyota(String sPlaca){
+        //Inicializando
+        super(sPlaca);
+        super.setdVelocidadMaxima(200);
+    }
+}
